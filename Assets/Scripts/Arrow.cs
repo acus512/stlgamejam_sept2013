a281a1346d0bced766f4a8b22637dd5ea8c5ef44
Brett Sweeney <brett@simutronics.com>
2013-09-13 23:17:36 -0500

    Better Archery

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -15,10 +15,21 @@ public class Arrow : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		//Face in the direction of travel
-		if (_rigidbody.velocity == Vector3.zero) {
+		if (_rigidbody.velocity == Vector3.zero || _rigidbody.isKinematic) {
 			return;
 		}
 
 		_transform.rotation = Quaternion.Slerp(_transform.rotation, Quaternion.LookRotation(_rigidbody.velocity, Vector3.up), 1);
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		_rigidbody.isKinematic = true;
+
+		var enemy = collision.gameObject.GetComponent<Enemy>();
+		if (enemy != null) {
+			//Deal damage to enemy
+			transform.parent = enemy.transform;
+		}
 	}
 }
