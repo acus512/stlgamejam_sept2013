7a60f14e9f8a4e809c497cb50bf657783bd93db7
CaliberMengsk <Caliber_mengsk@yahoo.com>
2013-09-15 12:57:59 -0700

    Added win condition
    Added instadeath
    Started work with in game menu

---
+++ Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InGameMenu : MonoBehaviour {
+	public Texture2D Checked;
+	public Texture2D Unchecked;
+	public Camera MainCamera;
+	public OVRCameraController riftCamera;
+	
+	private bool invert = false;
+	private bool oculus = false;
+	
+	void Start()
+	{
+		
+	}
+	
+	void Update()
+	{
+		if(Input.GetMouseButton(0))
+		{
+			RaycastHit hit;
+			
+			if(oculus)
+			{
+				if (Physics.Raycast(MainCamera.ScreenPointToRay(Input.mousePosition), out hit))
+				{	
+					if (hit.transform != null)
+					{
+						switch(hit.transform.name)
+						{
+							case "Invert Mouse":
+								Debug.Log("Inverting Mouse");
+								break;
+							case "Enable Rift":
+								riftCamera.enabled = false;
+								MainCamera.enabled = true;
+								Debug.Log("Toggling Rift");
+								break;
+						}
+						
+					}
+				}
+			}else{
+				if (Physics.Raycast(MainCamera.ScreenPointToRay(Input.mousePosition), out hit))
+				{
+					if (hit.transform != null)
+					{
+						switch(hit.transform.name)
+						{
+							case "Invert Mouse":
+								Debug.Log("Inverting Mouse");
+								break;
+							case "Enable Rift":
+								riftCamera.enabled = true;
+								MainCamera.enabled = false;
+								break;
+						}
+						
+					}
+				}	
+			}
+			
+		}
+	}
+}
---
+++ Assets/Scripts/WinScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinScript : MonoBehaviour {
+	
+	public bool won = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider c)
+	{
+		won = true;
+		Debug.Log("Won!");
+		Application.LoadLevel("Credits");
+	}
+	
+}
