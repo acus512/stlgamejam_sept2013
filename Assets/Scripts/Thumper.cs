a22fc2489aea5d8aba1695fb35c34e5b2e92f2f2
SinisterRouge <raymondw.phillips@yahoo.com>
2013-09-15 16:34:27 -0500

    Added Thumper prefab that kills anything that triggers it.

---
+++ Assets/Scripts/Thumper.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Thumper : MonoBehaviour {
+	
+	GameObject thumper;
+	Collider player;
+	
+	// Use this for initialization
+	void Start () {
+		thumper = transform.FindChild ("thumper_mesh").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter(Collider other) {
+    	if (thumper.animation.isPlaying == false)
+		{
+		
+			player = other;
+			thumper.animation.Play ("Take 001");
+			
+		}
+		else
+		{
+			if (collider.name == "Thumper")
+			{
+				player.gameObject.SendMessage("Die", SendMessageOptions.DontRequireReceiver);
+			}
+		}
+				
+			
+		
+    }
+	
+	
+	
+}
