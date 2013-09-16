cfdbfd89f5052316cc02a6b5784ba12a8f919e98
CaliberMengsk <Caliber_mengsk@yahoo.com>
2013-09-15 17:55:27 -0700

    Added in partial story sounds

--- Assets/Scripts/WinScript.cs
+++ Assets/Scripts/WinScript.cs
@@ -4,21 +4,35 @@ using System.Collections;
 public class WinScript : MonoBehaviour {
 	
 	public bool won = false;
+	float wait = 10;
+	float curTime = 0;
+	public Texture2D texture;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void OnGUI () 
+	{
+		if(won)
+		{
+			curTime += Time.deltaTime;
+			GUI.color = new Color (0,0,0,curTime/wait);
+			GUI.DrawTexture(new Rect(0,0,Screen.width, Screen.height),texture);
+			if(curTime >= wait)
+			{
+				if(bool.Parse(PlayerPrefs.GetString("won", "false")))
+				{
+					Application.LoadLevel("Credits");
+				}
+			}
+		}
 	}
 	
 	void OnTriggerEnter(Collider c)
 	{
 		won = true;
-		Debug.Log("Won!");
-		Application.LoadLevel("Credits");
 	}
 	
 }
